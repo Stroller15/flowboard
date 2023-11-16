e7f897f630e39e2d4c77ebe6024401f0491390fb
Stroller15 <shubhamverma6351@gmail.com>
2023-11-16 14:37:00 +0530

    feat: :sparkles: add flowboard button

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-
+import FlowBoard from "@/components/FlowBoard"
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <FlowBoard/>
   )
 }
--- components/FlowBoard.tsx
+++ components/FlowBoard.tsx
@@ -1,8 +1,43 @@
+import PlusIcon from "@/assets/PlusIcon"
+
 
 const FlowBoard = () => {
   return (
-    <div>FlowBoard</div>
-  )
-}
+    <div className="
+    m-auto
+    flex
+    min-h-screen
+    w-full
+    items-center
+    overflow-x-auto
+    overflow-y-hidden
+    px-[40px]
+    ">
+      <div className="m-auto">
+        <button className="
+        h-[60px]
+        w-[350px]
+        min-w-[350px]
+        cursor-pointer
+        rounded-lg
+        bg-buttonBackgroundColor
+        p-4
+        ring-amber-400
+        hover:ring-2
+        flex
+        gap-2
+        ">
+          <div className="w-6 h-6">
+          <PlusIcon/> 
+          </div>
+           
+            Add Column
+        </button>
+        </div>
+    </div>
+  );
+
+
+};
 
 export default FlowBoard
